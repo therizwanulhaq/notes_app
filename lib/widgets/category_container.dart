f23c4844bd260a4d7ca8ff49e6d965882970f6e6
rizwan <therizwanulhaq@gmail.com>
2024-08-13 12:12:46 +0530

    separated category container to add styles on it

---
+++ lib/widgets/category_container.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class CategoryContainer extends StatelessWidget {
+  const CategoryContainer({
+    super.key,
+    required this.category,
+  });
+
+  final String category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(right: 5),
+      padding: const EdgeInsets.all(10),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.secondaryContainer,
+        borderRadius: BorderRadius.circular(
+          10,
+        ),
+      ),
+      child: Text(
+        category,
+        style: const TextStyle(
+          color: Colors.black,
+        ),
+      ),
+    );
+  }
+}
