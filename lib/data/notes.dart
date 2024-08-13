2c28bdbbd8917cb59e705a646e4bddb0ef887326
rizwan <therizwanulhaq@gmail.com>
2024-08-13 12:11:41 +0530

    dummy data

---
+++ lib/data/notes.dart
@@ -0,0 +1,30 @@
+import 'package:notes_app/models/note.dart';
+
+final notes = [
+  Note(
+    title: "first note",
+    content: "content of the note which can be big",
+    date: DateTime.now(),
+  ),
+  Note(
+    title: "second note",
+    content:
+        "content of the note which can be big yeah and ig this one is the one which might give me headache yeah this is it ig we'll see now how it will work out alright",
+    date: DateTime.now(),
+  ),
+  Note(
+    title: "third note",
+    content: "content of the note which can be big",
+    date: DateTime.now(),
+  ),
+  Note(
+    title: "fourth note",
+    content: "content of the note which can be big",
+    date: DateTime.now(),
+  ),
+  Note(
+    title: "fifth note",
+    content: "content of the note which can be big",
+    date: DateTime.now(),
+  ),
+];
